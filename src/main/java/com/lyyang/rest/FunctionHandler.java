9f8ff6fbd2312b2705578d244f6e92496d694fb4
Li-Yu, Yang <tony830802@gmail.com>
2022-10-30 23:02:02 +0800

    add log

--- src/main/java/com/lyyang/rest/FunctionHandler.java
+++ src/main/java/com/lyyang/rest/FunctionHandler.java
@@ -1,5 +1,6 @@
 package com.lyyang.rest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -9,9 +10,12 @@ import reactor.core.publisher.Mono;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Service
+@Slf4j
 public class FunctionHandler {
-    public Mono<ServerResponse> getTime(ServerRequest serverRequest){
+    public Mono<ServerResponse> getTime(ServerRequest serverRequest) {
+
+        log.info("HIHI for getTime");
         return ok().contentType(MediaType.TEXT_PLAIN)
-                .body(Mono.just("Now is HIHI"),String.class);
+                .body(Mono.just("Now is HIHI"), String.class);
     }
 }
