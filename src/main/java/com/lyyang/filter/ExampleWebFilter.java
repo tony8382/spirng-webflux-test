27b7b8bd267119d42f11455d667adfb943c75958
Li-Yu, Yang <tony830802@gmail.com>
2023-01-14 13:02:21 +0800

    add  cors header

--- src/main/java/com/lyyang/filter/ExampleWebFilter.java
+++ src/main/java/com/lyyang/filter/ExampleWebFilter.java
@@ -1,7 +1,9 @@
 package com.lyyang.filter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -25,14 +27,30 @@ public class ExampleWebFilter implements WebFilter {
             return serverWebExchange.getResponse().setComplete();
 
         }
-        serverWebExchange.getResponse()
-                .getHeaders().add("web-filter", "web-filter-test");
+        HttpHeaders headers = serverWebExchange.getResponse()
+                .getHeaders();
+
+        ServerHttpRequest s = serverWebExchange.getRequest();
+        log.info("protocol:{}, host:{}, port:{}, path:{}", s.getURI().getScheme(), s.getURI().getHost(), s.getURI().getPort(), s.getPath().toString());
+
+        log.info("domain:{}", s.getURI());
+        setCorsHeader(headers, serverWebExchange.getRequest().getHeaders().getFirst("Origin"));
 
         Mono<Void> result = webFilterChain.filter(serverWebExchange);
 
 
-        return result.doOnSuccess((c)->{
+        return result.doOnSuccess((c) -> {
             log.info("HIHI S:{}", serverWebExchange.getResponse().getStatusCode());
         });
+    }
+
+    private void setCorsHeader(HttpHeaders headers, String origin) {
+
+        headers.add("access-control-allow-credentials", "true");
+        headers.add("access-control-allow-headers", "Content-Type");
+        headers.add("access-control-allow-methods", "*");
+        headers.add("access-control-allow-origin", "https://tw.yahoo.com");
+        headers.add("access-control-max-age", String.valueOf(24 * 60 * 60));
+
     }
 }
